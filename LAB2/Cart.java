b129909702b08ea6052534117770e94fa4fb20de
leedoekahn401 <ducanh4012006@gmail.com>
2025-10-19 09:25:19 +0700

    update

--- LAB2/Cart.java
+++ LAB2/Cart.java
@@ -1,52 +1,32 @@
 import java.util.LinkedList;
-public class Cart
-{
+
+public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
-    LinkedList<DigitalVideoDisc> itemsOrdered =  new LinkedList<>();
-    private int qtyOrdered = 0;
-    public void addDigitalVideoDisc(DigitalVideoDisc a)
-    {
-        if(qtyOrdered < MAX_NUMBERS_ORDERED)
-        {
+    LinkedList<DigitalVideoDisc> itemsOrdered = new LinkedList<>();
+
+    public void addDigitalVideoDisc(DigitalVideoDisc a) {
+        if (itemsOrdered.size() < MAX_NUMBERS_ORDERED) {
             itemsOrdered.add(a);
-            qtyOrdered++;
             System.out.println("The disc has been added.");
-            System.out.println("Cost: "+ totalCost());
-        }
-        if(MAX_NUMBERS_ORDERED - qtyOrdered < 3)
-        {
-            int b = MAX_NUMBERS_ORDERED - qtyOrdered;
-            System.out.println("The cart is almost full, there are "+ b +" slots left." );
-        }
-        if(qtyOrdered >= MAX_NUMBERS_ORDERED )
-        {
-            System.out.println("The cart is full!!!");
+            System.out.println("Cost: " + totalCost());
+        } else {
+            System.out.println("The cart is full!");
         }
     }
-    public double totalCost()
-    {
-        double sum =0;
-        for(int i =0;i<qtyOrdered;i++)
-        {
-            sum+= itemsOrdered.get(i).getCost();
+
+    public double totalCost() {
+        double sum = 0;
+        for (int i = 0; i < itemsOrdered.size(); i++) {
+            sum += itemsOrdered.get(i).getCost();
         }
         return sum;
     }
 
-    public void removeDigitalVideoDisc(DigitalVideoDisc a)
-    {
-        if(qtyOrdered <=0)
-        {
-            System.out.println("Cannot remove.");
-        }
-        if(qtyOrdered >0)
-        {
-            itemsOrdered.remove(a);
-            System.out.println("Successfully removed!!!");
+    public void removeDigitalVideoDisc(DigitalVideoDisc a) {
+        if (itemsOrdered.remove(a)) {
+            System.out.println("The disc has been removed.");
+        } else {
+            System.out.println("The disc is not in the cart.");
         }
     }
-
-    
-
-
 }
