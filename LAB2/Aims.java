f2c956ebb829289c5672eb35e93919028cfcf9e4
leedoekahn401 <ducanh4012006@gmail.com>
2025-10-19 09:43:11 +0700

    update

--- LAB2/Aims.java
+++ LAB2/Aims.java
@@ -1,18 +1,19 @@
-
-public class Aims
-{
+public class Aims {
     public static void main(String[] args) {
-        {
-            Cart anOrder = new Cart();
-            DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
-            DigitalVideoDisc dvd2 = new DigitalVideoDisc("title", "category" , "director", 90, 10.05f);
-            DigitalVideoDisc dvd3 = new DigitalVideoDisc("boom boom");
-            anOrder.addDigitalVideoDisc(dvd1);
-            anOrder.addDigitalVideoDisc(dvd2);
-            System.out.println("Total cost is: " + anOrder.totalCost());
-            anOrder.removeDigitalVideoDisc(dvd1);
-            System.out.println("Total cost is: " + anOrder.totalCost());    
+        Cart anOrder = new Cart();
 
-        }
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, (float)19.95);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, (float)24.95);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, (float)18.99);
+
+        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("Total cost is: " + anOrder.totalCost());
+
+        anOrder.removeDigitalVideoDisc(dvd1);
+
+        System.out.println("Total cost is: " + anOrder.totalCost());
     }
 }
--- LAB2/DigitalVideoDisc.java
+++ LAB2/DigitalVideoDisc.java
@@ -1,45 +1,35 @@
 
-public class DigitalVideoDisc
-{
+
+public class DigitalVideoDisc{
     private String title;
     private String category;
     private String director;
     private int length;
-    private float cost;
+    private float cost; 
 
-    public DigitalVideoDisc(String title, String category, String director, int length,float cost)
-    {
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost){
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
     }
-    public DigitalVideoDisc(String category)
-    {
-        super();
-        this.category=category;
-    }
     
-public String getTitle()
-{
-    return title;
-}
-public String getCategory()
-{
-    return category;
-}
-public String getDirector()
-{
-    return director;
-}
-public int length()
-{
-    return length;
-}
-public float getCost()
-{
-    return cost;
-}
+
+    public String getTitle(){
+        return title;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public String getDirector(){
+        return director;
+    }
+    public int length(){
+        return length;
+    }
+    public float getCost(){
+        return cost;
+    }
 
 }
