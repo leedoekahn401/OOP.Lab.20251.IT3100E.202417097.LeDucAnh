082b6bb62e0e3db3a09d773067508ce7264dcc3d
leedoekahn401 <ducanh4012006@gmail.com>
2025-10-30 13:34:49 +0700

    update

--- LAB3/Prj/src/DigitalVideoDisc.java
+++ LAB3/Prj/src/DigitalVideoDisc.java
@@ -1,35 +1,56 @@
 package Lab3.src;
-public class DigitalVideoDisc{
+public class DigitalVideoDisc {
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+    private int id;
+    private static int nbDigitalVideoDiscs = 0;
 
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost){
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
 
-    public String getTitle(){
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
         return title;
     }
-    public String getCategory(){
+
+    public String getCategory() {
         return category;
     }
-    public String getDirector(){
+
+    public String getDirector() {
         return director;
     }
-    public int length(){
+
+    public int getLength() {
         return length;
     }
-    public float getCost(){
+
+    public float getCost() {
         return cost;
     }
 
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
