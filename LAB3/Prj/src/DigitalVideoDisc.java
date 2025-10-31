169d34a17f28a2beb8d2300e42fdf8027983ca80
leedoekahn401 <ducanh4012006@gmail.com>
2025-11-01 00:46:00 +0700

    update

--- LAB3/Prj/src/DigitalVideoDisc.java
+++ LAB3/Prj/src/DigitalVideoDisc.java
@@ -14,6 +14,24 @@ public class DigitalVideoDisc {
         this.id = nbDigitalVideoDiscs;
     }
 
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
