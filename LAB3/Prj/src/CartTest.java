69716b152fd5136e002a19fa454d4155cf5ef479
leedoekahn401 <ducanh4012006@gmail.com>
2025-11-01 00:48:53 +0700

    update

--- LAB3/Prj/src/CartTest.java
+++ LAB3/Prj/src/CartTest.java
@@ -5,25 +5,22 @@ public class CartTest {
 
         Cart cart = new Cart();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-                "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         cart.addDigitalVideoDisc(dvd1);
 
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
-                "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
         cart.addDigitalVideoDisc(dvd2);
 
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
-                "Animation", 18.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation", 18.99f);
         cart.addDigitalVideoDisc(dvd3);
 
         cart.printCart();
 
-        System.out.println("\n===== TEST SEARCH BY ID =====");
+        System.out.println("\nTEST SEARCH BY ID");
         cart.searchById(2);
         cart.searchById(10);
 
-        System.out.println("\n===== TEST SEARCH BY TITLE =====");
+        System.out.println("\nTEST SEARCH BY TITLE");
         cart.searchByTitle("Aladin");
         cart.searchByTitle("Frozen");
     }
